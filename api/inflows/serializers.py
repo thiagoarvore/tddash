d19c6a158eecfa0fef2ff759ff2768dbe1c2b4b7
Thiago Azevedo Rodrigues de Oliveira <thiago.oliveira@tecno.com.br>
2025-01-14 11:15:11 -0300

    feature: serializers

---
+++ api/inflows/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Inflow
+
+
+class InflowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inflow
+        fields = ('product', 'supplier', 'quantity', 'cost_price',)
---
+++ api/outflows/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Outflow
+
+
+class OutflowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Outflow
+        fields = ('product', 'implemented', 'quantity', 'selling_price',)
